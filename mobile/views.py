75078f300a0567d3cdef7b49d54b0f46727739a3
liu-xing.1234@163.com <liu-xing.1234@163.com>
2017-05-31 15:39:57 +0800

    '0531commit'

--- mobile/views.py
+++ mobile/views.py
@@ -1,3 +1,11 @@
+#coding=utf-8
 from django.shortcuts import render
-
+from django.template.loader import get_template
+from django.http import HttpResponse
+from mobile import models
 # Create your views here.
+def index(request):
+    template = get_template('mobile/index.html')
+    products = models.Product.objects.all()
+    html = template.render(locals())
+    return HttpResponse(html)
--- mysite/settings.py
+++ mysite/settings.py
@@ -115,7 +115,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+FILE_CHARSET = 'gbk'
+DEFAULT_CHARSET = 'utf-8'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
